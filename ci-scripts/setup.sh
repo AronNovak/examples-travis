7500ba753461dd4cd94271fc55348b3858fae1f6
Aron Novak <aron@gizra.com>
2017-08-03 00:03:29 +0200

    ci-scripts/setup.sh: improved comments

--- ci-scripts/setup.sh
+++ ci-scripts/setup.sh
@@ -3,11 +3,14 @@
 composer global require drush/drush
 export PATH=$PATH:~/.composer/vendor/bin
 
+# MySQL gone away error mitigation.
 mysql -e "SET GLOBAL wait_timeout = 36000;"
 mysql -e "SET GLOBAL max_allowed_packet = 134209536;"
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
 
+# Database for Drupal.
 mysql -e 'CREATE DATABASE drupal'
 
+# Vanilla Drupal 8 core.
 drush --yes core-quick-drupal --core=drupal-8.x --profile=normal --no-server --db-url=mysql://root:@127.0.0.1/drupal
