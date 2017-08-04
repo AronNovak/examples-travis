d9f2bca526ac9cce4dabe3252e72879418e827dd
Aron Novak <aron@gizra.com>
2017-08-04 16:37:45 +0200

    initial full setup process for the module inside a D8 installation

--- ci-scripts/setup.sh
+++ ci-scripts/setup.sh
@@ -13,4 +13,17 @@ mysql -e "SHOW VARIABLES LIKE 'wait_timeout';"
 mysql -e 'CREATE DATABASE drupal'
 
 # Vanilla Drupal 8 core.
+PHP_OPTIONS="-d sendmail_path=$(which true)"
+export PHP_OPTIONS
 drush --yes core-quick-drupal --core=drupal-8.3.x --profile=standard --no-server --db-url=mysql://root:@127.0.0.1/drupal testing
+
+# Activates the examples module.
+DRUPAL_ROOT=testing/drupal-7.x
+export DRUPAL_ROOT
+ln -s $(pwd) $DRUPAL_ROOT/modules/examples
+cd $DRUPAL_ROOT || exit 1
+drush --yes pm-enable examples fapi_example
+
+# Create the webserver for WDIO test
+drush runserver 127.0.0.1:8080 &
+until (curl --output /dev/null --silent --head --fail http://localhost:8080); do sleep 1; done
