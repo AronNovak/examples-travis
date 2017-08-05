a8267bb7bc72950b0c7ba9f8373b897e2785ff11
Aron Novak <aron@gizra.com>
2017-08-05 13:09:01 +0200

    tests/wdio/wdio.conf.travis.js: fix base URL

--- tests/wdio/wdio.conf.travis.js
+++ tests/wdio/wdio.conf.travis.js
@@ -57,7 +57,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'http://server.local',
+    baseUrl: 'http://127.0.0.1:8080',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 20000,
